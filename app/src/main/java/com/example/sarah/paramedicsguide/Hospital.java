a2256b3a00a9ba7e624904f6c540cc4320b9ed65
mariabuk1112 <47556312+mariabuk1112@users.noreply.github.com>
2019-03-14 20:44:34 +0300

    filtring search map

--- app/src/main/java/com/example/sarah/paramedicsguide/Hospital.java
+++ app/src/main/java/com/example/sarah/paramedicsguide/Hospital.java
@@ -1,50 +1,115 @@
 package com.example.sarah.paramedicsguide;
+public class Hospital {
+    String id;
+    String name;
+    double locationX;
+    double locationY;
+    long phone;
+    String password;
+    String category;
+    String email;
 
-public class Hospital {String hospitalName;
-    String hospitalID;
-    String hospitalEmail;
-    String hospitalPassword;
 
     public Hospital() {
     }
 
-    public Hospital(String hospitalName, String hospitalID, String hospitalEmail, String hospitalPassword) {
-        this.hospitalName = hospitalName;
-        this.hospitalID = hospitalID;
-        this.hospitalEmail = hospitalEmail;
-        this.hospitalPassword = hospitalPassword;
+    public Hospital(String id) {
+        this.id = id;
+    }
+
+    public Hospital(String id, String name, double locationX, double locationY, long phone, String password , String category) {
+        this.id = id;
+        this.name = name;
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.phone = phone;
+        this.password = password;
+        this.category = category;
+    }
+
+    public Hospital(String name,String id,  String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLocationX() {
+        return locationX;
+    }
+    public double getLocationY() {
+        return locationY;
     }
 
 
-    public String getHospitalName() {
-        return hospitalName;
+    public long getPhone() {
+        return phone;
     }
 
-    public void setHospitalName(String hospitalName) {
-        this.hospitalName = hospitalName;
+
+
+
+
+    public String getPassword() {
+        return password;
     }
 
-    public String getHospitalID() {
-        return hospitalID;
+
+
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public void setHospitalID(String hospitalID) {
-        this.hospitalID = hospitalID;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getHospitalEmail() {
-        return hospitalEmail;
+    public void setLocationX(double locationX) {
+        this.locationX = locationX;
+    }
+    public void setLocationY(double locationY) {
+        this.locationY = locationY;
     }
 
-    public void setHospitalEmail(String hospitalEmail) {
-        this.hospitalEmail = hospitalEmail;
+    public void setPhone(long phone) {
+        this.phone = phone;
     }
 
-    public String getHospitalPassword() {
-        return hospitalPassword;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
-    public void setHospitalPassword(String hospitalPassword) {
-        this.hospitalPassword = hospitalPassword;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public void logIn(int id, String password) {
+
+    }
+
+    public void logOut(int id) {
+
+    }
+
+    public void modifyHospitalEmail( String email) {
+
+    }
+
+    public void modifyHospitalPassword( String password) {
+
     }
 }
+
