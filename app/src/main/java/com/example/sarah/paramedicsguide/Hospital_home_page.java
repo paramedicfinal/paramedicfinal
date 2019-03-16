410e57f2d18d6e076b0e4b652d69ed52659111f6
mariabuk1112 <47556312+mariabuk1112@users.noreply.github.com>
2019-03-16 08:26:19 +0300

    style of pg

---
+++ app/src/main/java/com/example/sarah/paramedicsguide/Hospital_home_page.java
@@ -0,0 +1,17 @@
+package com.example.sarah.paramedicsguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Hospital_home_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_home_page);
+
+
+    }
+}
